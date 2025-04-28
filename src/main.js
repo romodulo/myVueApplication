e7d5c24c7dfc82bf5ba1eb19261faacf7e0892cb
romel-romodulo <57033860+romodulo@users.noreply.github.com>
2025-04-28 12:41:48 +1000

    First commit of < 4-28>  <12:39>: empty out package -- myVueApplication

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+// import { createPinia } from 'pinia' //commented-out, because not-using--for-now
 
 import App from './App.vue'
-import router from './router'
+// import router from './router' //commented-out, because not-using--for-now
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+// app.use(createPinia()) //commented-out, because not-using--for-now
+// app.use(router) //commented-out, because not-using--for-now
 
 app.mount('#app')
